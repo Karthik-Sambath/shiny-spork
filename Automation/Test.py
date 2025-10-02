8f7ea6dc3ec7d87c2afa7518e865abdac6d425d5
Karthik Sambath <karthiksambath.konar@gmail.com>
2025-10-02 22:39:37 +0530

    Added new file Test.py to open directory

---
+++ Automation/Test.py
@@ -0,0 +1,8 @@
+# Open the file explorer and navigate to the specified directory
+import os
+
+# Specify the directory you want to open
+directory = "C:/path/to/your/directory"
+
+# Open the file explorer
+os.startfile(directory)
